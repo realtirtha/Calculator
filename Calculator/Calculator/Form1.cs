d7a73f54b45e545041e57b4c011992372e93f785
Tirth <tirthacodes@gmail.com>
2022-12-08 21:49:03 +0545

    all things reset

--- Calculator/Calculator/Form1.cs
+++ Calculator/Calculator/Form1.cs
@@ -2,10 +2,7 @@ namespace Calculator
 {
     public partial class Form1 : Form
     {
-        int sign = 0;
-        int add = 0;
-        string first = "0", second = "0";
-        int f, s;
+        
 
 
 
@@ -25,34 +22,8 @@ namespace Calculator
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if (add == 0)
-            {
-                if (sign == 0)
-                {
-                    if (Display.Text == "START")
-                    {
-                        Display.Text = "1";
-                    }
-                    else
-                    {
-                        Display.Text += "1";
-                    }
-                }
-            }
-            else if(add == 1)
-            {
-                if (sign == 0)
-                {
-                    Display.Text += "1";
-                }
-                else if (sign == 1)
-                {
-                first = Display.Text;
-                Display.Text = "1";
-                sign = 0;
-                }
-
-            }
+            
+            
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -78,36 +49,22 @@ namespace Calculator
         }
         private void btnequals_Click(object sender, EventArgs e)
         {
-            second = Display.Text;
-            f = Convert.ToInt32(first);
-            s = Convert.ToInt32(second);
-
-            if (add == 1) Add(f, s);
-
-            f = 0;
-            s = 0;
+            
         }
 
         
 
         private void btnAC_Click(object sender, EventArgs e)
         {
-            Display.Text = "0";
+            
         }
 
         private void btnPlus_Click(object sender, EventArgs e)
         {
-            sign = 1;
-            add = 1;
+            
         }
 
-        private void Add(int x, int y)
-        {
-            int result = f + s;
-            Display.Text = result.ToString();
-            add = 0;
-            sign = 1;
-        }
+        
 
        
     }
