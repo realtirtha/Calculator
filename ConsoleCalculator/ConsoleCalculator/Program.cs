2f22d96cceac62f595a82546d15c71b5f689ab82
Tirth <tirthacodes@gmail.com>
2022-12-16 21:47:38 +0545

    implemented try catch

--- ConsoleCalculator/ConsoleCalculator/Program.cs
+++ ConsoleCalculator/ConsoleCalculator/Program.cs
@@ -5,10 +5,9 @@
 
         static void Main(string[] args)
         {
-            String option;
+            var option = "n";
             do
-            {
-                
+            {             
                 double num1 = 0;
                 double num2 = 0;
                 double result = 0;
@@ -17,11 +16,9 @@
                 Console.WriteLine("Calculator program");
                 Console.WriteLine("--------------------");
 
-                Console.Write("Enter first number : ");
-                num1 = Convert.ToDouble(Console.ReadLine());
+                num1 = readOne(num1);
+                num2 = readTwo(num2);
 
-                Console.Write("Enter second number: ");
-                num2 = Convert.ToDouble(Console.ReadLine());
 
                 Console.WriteLine("OPERATIONS: ");
                 Console.WriteLine("\t+ : Add");
@@ -55,11 +52,42 @@
                         Console.WriteLine("Not a valid option.");
                         break;
                 }
-                Console.WriteLine("Would you like to continue again?");
-                Console.WriteLine("Click Y to continue");
-                option = Console.ReadLine().ToString().ToUpper();
+                Console.WriteLine("\nDo you want to run once more? (y/n)");
+                option = Console.ReadLine();
 
-            } while (option == "Y") ;
+
+            } while (option.ToUpper() == "Y") ;
+
+            Console.WriteLine("Ba-Bye");
+        }
+        
+        static double readOne(double n1)
+        {
+            try
+            {
+                Console.Write("Enter first number : ");
+                n1 = Convert.ToDouble(Console.ReadLine());
+                return n1;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Enter numbers only.");
+                return readOne(n1);
+            }
+        }
+        static double readTwo(double n2)
+        {
+            try
+            {
+                Console.Write("Enter second number : ");
+                n2 = Convert.ToDouble(Console.ReadLine());
+                return n2;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Enter numbers only.");
+                return readOne(n2);
+            }
         }
     }
 }
